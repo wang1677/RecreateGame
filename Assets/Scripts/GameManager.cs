c04a92a8c426b72cdc33ad957efb89b422243945
Zhen Wang <wangzhenaph@gmail.com>
2023-11-02 14:45:32 +1100

    setting the start scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ghost[] ghosts;
+    public PacStudent pacStudent;
+    public Transform pellets;
+    public int score { get; private set; }
+    public int lives { get; private set;}
+
+    private void Start()
+    {
+        NewGame();
+    }
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+    private void NewRound()
+    {
+        foreach(Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+        this.pacStudent.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+        this.pacStudent.gameObject.SetActive(false);
+    }
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhostEat(Ghost ghost)
+    {
+        SetScore(this.score + ghost.points);
+    }
+
+    public void PacStudentEast()
+    {
+        this.pacStudent.gameObject.SetActive(false);
+       
+        SetLives(this.lives - 1);
+        
+        if (this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 2.5f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+}
+
